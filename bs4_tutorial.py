270ba09b26843dfab32ed529dcd829289734de24
mivanov06 <mivanov06@yandex.ru>
2023-06-13 17:20:05 +1000

    download books with titles

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -1,4 +1,4 @@
-from urllib import parse
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -6,31 +6,40 @@ from requests import HTTPError
 
 
 def check_for_redirect(response):
-    if response.history and response.history[0].status_code in [300, 301]:
-        raise requests.exceptions.HTTPError
+    if response.status_code in [301, 302]:
+        raise HTTPError
 
 
 def parse_book_page(url, book_id):
     response = requests.get(f'{url}/b{book_id}/', allow_redirects=False)
     response.raise_for_status()
-    try:
-        check_for_redirect(response)
-    except HTTPError:
-        return 'None'
-
+    check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
+
     title, author = soup.find(id='content').find('h1').text.split('::')
+
     image_url = soup.find(class_='bookimage').find('img')['src']
-    image_url = f"{url}/{image_url}"
+    if image_url == '/images/nopic.gif':
+        image_url = None
+    else:
+        image_url = f"{url}{image_url}"
+
     post_text = soup.findAll(class_='d_book')[2].find('tr').find('td').text
-    full_text_url = f'{url}/txt.php?id={book_id}'
+
+    full_text_url = soup.find(href=re.compile('txt.php'))['href']
+
+    if full_text_url:
+        full_text_url = f'{url}{full_text_url}'
+    else:
+        full_text_url = None
+    print(full_text_url)
     return {
         'title': title.strip(),
         'author': author.strip(),
         'image_url': image_url,
         'post_text': post_text,
         'full_text_url': full_text_url
-        }
+    }
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import requests
+from requests import HTTPError
 
 from bs4_tutorial import parse_book_page, check_for_redirect
 from pathvalidate import sanitize_filename
@@ -11,9 +12,8 @@ def download_file(url, filename, folder='books', payload={}):
     response.raise_for_status()
     check_for_redirect(response)
 
-    sanitazed_filename = sanitize_filename(filename)
     os.makedirs(folder, exist_ok=True)
-    filepath = os.path.join(folder, sanitazed_filename)
+    filepath = os.path.join(folder, filename)
 
     with open(filepath, 'wb') as file:
         file.write(response.content)
@@ -23,7 +23,14 @@ def download_file(url, filename, folder='books', payload={}):
 if __name__ == '__main__':
     url = 'https://tululu.org'
     books_folder = 'books'
-    book_id = 1
-    parse_book_page = parse_book_page(url, book_id)
-    filepath = download_file(parse_book_page['full_text_url'], parse_book_page['title'], books_folder)
-
+    for book_id in range(1, 11):
+        print(book_id)
+        try:
+            parse_page = parse_book_page(url, book_id)
+            filename = f"{book_id}. {sanitize_filename(parse_page['title'])}"
+            # print(f"{parse_page['full_text_url']=}\n"
+            #       f"{parse_page['image_url']=}")
+            filepath = download_file(parse_page['full_text_url'], filename, books_folder)
+        except:
+            print('Книга не скачана')
+            continue
